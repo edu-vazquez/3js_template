7b6456ef86b138e31aff21617d8428a94a49e7b1
edu-vazquez <dev.eduardovazquez@gmail.com>
2024-10-21 23:07:07 +0200

    upload

---
+++ src/components/Scene/Script.jsx
@@ -0,0 +1,74 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+//Global variables
+let currentRef = null;
+
+//Scene, camera, renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(25, 100 / 100, 0.1, 100);
+scene.add(camera);
+camera.position.set(5, 5, 5);
+camera.lookAt(new THREE.Vector3());
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(100, 100);
+
+//OrbitControls
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.enableDamping = true;
+
+//Resize canvas
+const resize = () => {
+  renderer.setSize(currentRef.clientWidth, currentRef.clientHeight);
+  camera.aspect = currentRef.clientWidth / currentRef.clientHeight;
+  camera.updateProjectionMatrix();
+};
+window.addEventListener("resize", resize);
+
+//Animate the scene
+const animate = () => {
+  orbitControls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+};
+animate();
+
+//cube
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial()
+);
+scene.add(cube);
+
+//Init and mount the scene
+export const initScene = (mountRef) => {
+  currentRef = mountRef.current;
+  resize();
+  currentRef.appendChild(renderer.domElement);
+};
+
+//Dismount and clena up the buffer from the scene
+export const cleanUpScene = () => {
+  scene.traverse((object) => {
+    // Limpiar geometrías
+    if (object.geometry) {
+      object.geometry.dispose();
+    }
+
+    // Limpiar materiales
+    if (object.material) {
+      if (Array.isArray(object.material)) {
+        object.material.forEach((material) => material.dispose());
+      } else {
+        object.material.dispose();
+      }
+    }
+
+    // Limpiar texturas
+    if (object.material && object.material.map) {
+      object.material.map.dispose();
+    }
+  });
+  currentRef.removeChild(renderer.domElement);
+};
